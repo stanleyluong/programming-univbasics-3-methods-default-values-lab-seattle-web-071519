94b6a53ff1bc81cf6fa9e8172e7c72b0aaf4d1d6
crisp-convention-4859 <slluong@uw.edu>
2019-07-03 10:07:18 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,10 @@
 # Your code here
+veg1 = broccoli
+veg2 = macaroni
+protein = meat
+def meal_choice(veg1, veg2, protein)
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice
